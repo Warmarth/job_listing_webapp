1587e9d4ebc405c28a15984445b768cd33d6fd24
Nehal_Sahu <ProfessionalNehal@outlook.com>
2023-09-28 01:48:33 +0530

    Fixed Header Bg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import SelectionBox from "./subcomponents/SelectionBox";
 export default function Header({ info, onKeywordSelect }) {
   return (
     <header>
-      <div className="relative -z-10 min-h-[11.5rem] bg-[var(--Desaturated-Dark-Cyan)] bg-[url('/src/assets/images/bg-header-mobile.svg')] bg-cover bg-center bg-no-repeat sm:bg-[url('/src/assets/images/bg-header-desktop.svg')]"></div>
+      <div className="relative -z-10 min-h-[11.5rem] bg-[var(--Desaturated-Dark-Cyan)] bg-[url('/images/bg-header-mobile.svg')] bg-cover bg-center bg-no-repeat sm:bg-[url('/images/bg-header-desktop.svg')]"></div>
       <SelectionBox info={info} onKeywordSelect={onKeywordSelect} />
     </header>
   );
